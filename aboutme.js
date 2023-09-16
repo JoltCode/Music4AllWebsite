aae4c788832196b4a52fabcf8a42f899a0b3219f
Soontosh <sapatapati@gmail.com>
2023-09-16 15:00:08 -0500

    Adding Intial Stuff

---
+++ aboutme.js
@@ -0,0 +1,20 @@
+//Add more animations for the intro stuff
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
+
+const infoContainer = document.getElementById("scrollSvg")
+    infoContainer.addEventListener("click", (e) => {
+        //scrollTo(0, window.innerHeight + 1)
+        $('html, body').animate({
+            scrollTop: window.innerHeight + 1
+    }, 500)
+})
